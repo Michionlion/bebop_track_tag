0197b73180a18dccd6ddee16cddd8d84e65126b0
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2017-03-30 17:51:22 -0400

    updates

--- src/track_tag.cpp
+++ src/track_tag.cpp
@@ -22,53 +22,56 @@ typedef struct vec{
 	double x,y,z;
 } vec;
 
-vec offset(0.4,0.0,0.0);
-double move_speed = 1; // higher than 1 = slower, lower than 1 = inaccurate but faster
+vec offset(0.0,0.0,0.2);
+double move_speed = 5;
 geometry_msgs::Twist move;
 
 
 void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
 	//markers is a vector<AlvarMarker>
-	if(msg->markers.empty()) return;
-	for(int i=0; i < msg->markers.size(); i++) {
-		ar_track_alvar_msgs::AlvarMarker marker = msg->markers.at(i);
-
-		if(marker.id == 8) {
-			geometry_msgs::Pose pose = marker.pose.pose;
-
-			double forward = pose.position.x - offset.x;
-			double left = -(pose.position.y - offset.y);
-			double up = pose.position.z - offset.z;
-
-			double dis = sqrt(forward*forward+left*left+up*up);
-
-			//normalize
-			forward /= dis * move_speed;
-			left /= dis * move_speed;
-			up /= dis * move_speed;
-
-			fprintf(stdout, "---\nDIS: %f\nPOS:\nX: %f\nY: %f\nZ: %f\n\n", dis, pose.position.x, pose.position.y, pose.position.z);
-			fprintf(stdout, "MOV:\nforward: %f\nleft: %f\nup: %f\n---\n", forward, left, up);
-
-			move.linear.x = forward;
-			move.linear.y = left;
-			move.linear.z = up;
-			move.angular.z = 0;
-			bebop_velocity.publish(move);
-
-		}
-
-
+	if(msg->markers.empty()) {
+		move.linear.x = 0;
+		move.linear.y = 0;
+		move.linear.z = 0;
+		move.angular.z = 0;
+		bebop_velocity.publish(move); //zero out
+		return;
 	}
+
+	ar_track_alvar_msgs::AlvarMarker marker = msg->markers.at(0);
+	geometry_msgs::Pose pose = marker.pose.pose;
+
+	double forward = pose.position.z - offset.z;
+	double left = pose.position.x - offset.x;
+	double up = pose.position.y - offset.y;
+
+	double dis = sqrt(forward*forward+left*left+up*up);
+
+	forward *= move_speed;
+	left *= move_speed;
+	up *= move_speed; // don't scale up, since it is in meters/sec
+
+	fprintf(stdout, "---\nDIS: %f\nPOS:\nX: %f\nY: %f\nZ: %f\n\n", dis, pose.position.x, pose.position.y, pose.position.z);
+	fprintf(stdout, "MOVE:\nforward: %f\nleft: %f\nup: %f\n---\n", forward, left, up);
+
+	move.linear.x = forward;
+	move.linear.y = left;
+	move.linear.z = up;
+	// move.linear.x = forward > 0.15 ? 1.0 : 0.0;
+	// move.linear.y = left > 0.15 ? 1.0 : 0.0;
+	// move.linear.z = up > 0.15 ? 1.0 : 0.0;
+	move.angular.z = 0;
+	bebop_velocity.publish(move);
+
 }
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "track_tag");
 	ros::NodeHandle nh;
 
-	ros::Rate r(30);
+	ros::Rate r(60);
 
-	bebop_velocity = nh.advertise<geometry_msgs::Twist>("bebop/debug_twist", 1);
+	bebop_velocity = nh.advertise<geometry_msgs::Twist>("bebop/cmd_vel", 1);
 	ar_pose = nh.subscribe("ar_pose_marker", 1, poseCallback);
 	geometry_msgs::Twist vel;
 
@@ -79,6 +82,6 @@ int main(int argc, char** argv) {
 	}
 
 
-	ros::shutdown();
+	// ros::shutdown();
 	return 0;
 }
