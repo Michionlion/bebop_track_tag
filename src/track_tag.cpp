610ebd780c258e6875248897d1cda64b7fee2ecc
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2017-03-30 01:45:35 -0400

    realization: markers is a vector, despite being declared like a c-style array in the msg file

--- src/track_tag.cpp
+++ src/track_tag.cpp
@@ -13,19 +13,18 @@ ros::Duration interval(5.0);
 bool visible;
 
 void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
-	//if(!visible) {
-	//	ROS_ERROR("MARKER NOT VISIBLE");
-	//	return;
-	//}
 
-	ROS_INFO("ID: %d", msg->markers[0].id);
+	//markers is a vector<AlvarMarker>
+
+	if(msg->markers.size() > 0) {
+
+		ROS_INFO("ID: %d", msg->markers[0].id);
+	}
 }
 
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "track_tag");
-	ros::NodeHandle nh;
-
-	tf::TransformListener tfl;
+	ros::NodeHandle nh;s
 
 	ros::Rate r(30);
 
@@ -33,17 +32,14 @@ int main(int argc, char** argv) {
 	ar_pose = nh.subscribe("ar_pose_marker", 1, poseCallback);
 	geometry_msgs::Twist vel;
 
+	ros::spin();
+
 	while(ros::ok()) {
 		try {
-			tfl.lookupTwist("camera_optical", "ar_marker_8", ros::Time(0), interval, vel);
-			bebop_velocity.publish(vel);
 
-			visible = true;
 		} catch(tf2::LookupException ex) {
-			visible = false;
 			ROS_ERROR("Exception: %s", ex.what());
 		} catch(tf2::ExtrapolationException ex) {
-			visible = false;
 			ROS_ERROR("Exception: %s", ex.what());
 		}
 		r.sleep();
