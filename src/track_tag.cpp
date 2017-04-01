c3a10dd3fa7b40509849aadfbf61363f43f82fb7
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2017-03-31 20:44:09 -0400

    updates

--- src/track_tag.cpp
+++ src/track_tag.cpp
@@ -1,21 +1,22 @@
 #include <ros/ros.h>
 #include <tf/transform_listener.h>
+#include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <ar_track_alvar_msgs/AlvarMarkers.h>
 #include <ar_track_alvar_msgs/AlvarMarker.h>
 #include <stdio.h>
-#include <string.h>
 
-#define MAX_SPEED 0.1
+#define MAX_SPEED 0.08
 #define DEADZONE 0.0025
 
 void clamp(double&, double, double);
 void dead(double&, double);
 void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr&);
 
-ros::Publisher bebop_velocity;
+ros::Publisher cmd_vel;
 ros::Subscriber ar_pose;
+ros::Subscriber odom;
 bool visible;
 
 typedef struct vec{
@@ -29,24 +30,24 @@ typedef struct vec{
 
 vec offset(0.0,0.0,0.75);
 geometry_msgs::Twist move;
-int backCount;
+geometry_msgs::Twist velocity;
+int backCount = 10;
 double lastBack;
 
 void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
 	//markers is a vector<AlvarMarker>
 	if(msg->markers.empty()) {
-		if((ros::Time::now().toSec() - lastBack) > 1.0 && backCount < 10) {
-			move.linear.x = -0.1;
+		if((ros::Time::now().toSec() - lastBack) > .4 && backCount < 10) {
+			move.linear.x = -0.35;
 			move.linear.y = 0;
 			move.linear.z = 0;
 			move.angular.z = 0;
-			bebop_velocity.publish(move);
+			cmd_vel.publish(move);
 			ROS_INFO("BACKING UP!");
 
 			lastBack = ros::Time::now().toSec();
 			backCount++;
 		}
-		ROS_ERROR("OUT OF VIEW!");
 		return;
 	}
 
@@ -54,10 +55,10 @@ void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
 
 	ar_track_alvar_msgs::AlvarMarker marker = msg->markers.at(0);
 	geometry_msgs::Pose pose = marker.pose.pose;
-
-	double forward = pose.position.z - offset.z;
-	double left = -(pose.position.x - offset.x);
-	double up = -(pose.position.y - offset.y);
+	double x,y,z;
+	double forward = z =pose.position.z - offset.z;
+	double left = x = -(pose.position.x - offset.x);
+	double up = y = -(pose.position.y - offset.y);
 
 	double dis = sqrt(forward*forward+left*left+up*up);
 	double sp = 0.8;
@@ -67,19 +68,36 @@ void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
 		return;
 	}
 
-	forward = forward > 0 ? sp*forward*forward : -sp*forward*forward;
-	left = left > 0 ? 1.25*sp*left*left : -1.25*sp*left*left;
-	up = up > 0 ? sp*up*up : -sp*up*up;
+	double p = 1.5;
+	forward = forward > 0 ? sp*pow(fabs(forward), p) : -sp*pow(fabs(forward), p);
+	left = left > 0 ? sp*pow(fabs(left), p) : -sp*pow(fabs(left), p);
+	up = up > 0 ? 1.5*sp*pow(fabs(up), p) : -1.5*sp*pow(fabs(up), p);
 
 	//apply clamp and thresh
 	clamp(forward, -MAX_SPEED, MAX_SPEED);
 	clamp(left, -MAX_SPEED, MAX_SPEED);
-	clamp(up, -MAX_SPEED, MAX_SPEED);
+	clamp(up, -MAX_SPEED*3, MAX_SPEED*3);
 	dead(forward, DEADZONE);
 	dead(left, DEADZONE);
 	dead(up, DEADZONE);
 
-	fprintf(stdout, "---\nDIS: %f\nPOS:\nX: %f\nY: %f\nZ: %f\n\n", dis, pose.position.x, pose.position.y, pose.position.z);
+	double lim = 0.1;
+	if(fabs(velocity.linear.x) > lim) {
+		forward = 0;
+		fprintf(stdout, "CUT FORWARD\n");
+	}
+	if(fabs(velocity.linear.y) > lim) {
+		left = 0;
+		fprintf(stdout, "CUT LEFT\n");
+	}
+	if(fabs(velocity.linear.z) > lim) {
+		up = 0;
+		fprintf(stdout, "CUT UP\n");
+	}
+
+	fprintf(stdout, "---\nPOS:\nX: %f\nY: %f\nZ: %f\n\n", pose.position.x, pose.position.y, pose.position.z);
+	fprintf(stdout, "VEL:\nforward: %f\nleft: %f\nup: %f\n\n", velocity.linear.x, velocity.linear.y, velocity.linear.z);
+	fprintf(stdout, "DIS: %f\nOFFSET:\nforward: %f\nleft: %f\nup: %f\n\n", dis, z, x, y);
 	fprintf(stdout, "MOVE:\nforward: %f\nleft: %f\nup: %f\n---\n", forward, left, up);
 
 
@@ -93,7 +111,7 @@ void poseCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg) {
 	// move.linear.z = up > 0.05 ? sp : 0.0;
 	move.angular.z = 0;
 	ROS_INFO("Publishing");
-	bebop_velocity.publish(move);
+	cmd_vel.publish(move);
 
 }
 
@@ -106,15 +124,19 @@ void dead(double& toDead, double deadThresh) {
 	if(toDead < deadThresh && toDead > -deadThresh) toDead = 0.0;
 }
 
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+	velocity = msg->twist.twist;
+}
+
 int main(int argc, char** argv) {
 	ros::init(argc, argv, "track_tag");
 	ros::NodeHandle nh;
 
 	// ros::Rate r(60);
 
-	bebop_velocity = nh.advertise<geometry_msgs::Twist>("bebop/cmd_vel", 1);
+	cmd_vel = nh.advertise<geometry_msgs::Twist>("bebop/cmd_vel", 1);
 	ar_pose = nh.subscribe("ar_pose_marker", 1, poseCallback);
-	geometry_msgs::Twist vel;
+	odom = nh.subscribe("bebop/odom", 1, odomCallback);
 
 	ros::spin();
 
